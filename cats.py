425c8a83b13486662e3acadada0ba83f68bed09a
jenjnif <jenjnif@hotmail.com>
2020-04-03 19:36:20 +0100

    Add skeleton idea for initial code and tests

---
+++ cats.py
@@ -0,0 +1,10 @@
+'''
+Using namedtuples.
+
+Cat with a:
+    - species_name
+    - size
+    - favourite_food
+'''
+
+from collections import namedtuple
---
+++ test_cats.py
@@ -0,0 +1,17 @@
+# four example tests
+
+
+def test_cat_defaults():
+    pass
+
+
+def test_cat_attributes():
+    pass
+
+
+def test_asdict():
+    pass
+
+
+def test_cat_replace():
+    pass
