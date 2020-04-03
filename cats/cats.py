e56e065a70e4df6592b1370987d0b690b65e8f6a
jenjnif <jenjnif@hotmail.com>
2020-04-03 21:02:26 +0100

    write four tests that currently pass

--- cats/cats.py
+++ cats/cats.py
@@ -13,14 +13,3 @@ from collections import namedtuple
 Cat = namedtuple('Cat', ['type', 'max_size', 'favourite_food', 'roars'])
 # sets the defaults for a new Cat if none are given
 Cat.__new__.__defaults__ = ('Unknown', 'Unknown', 'Unknown', False)
-
-c1 = Cat()
-print(c1)
-c2 = Cat('tiger', 3.9, 'deer', True)
-print(c2)
-c3 = Cat('lion', 2.5, 'elephant', False)
-print(c3)
-c3 = c3._replace(roars=True)
-print(c3)
-
-print('done')
--- cats/test_cats.py
+++ cats/test_cats.py
@@ -1,20 +1,41 @@
-import cats
+from cats import Cat
 
 # four example tests
+# Cat = namedtuple('Cat', ['type', 'max_size', 'favourite_food', 'roars'])
 
 
 def test_cat_defaults():
     c1 = Cat()
-
+    c2 = Cat('Unknown', 'Unknown', 'Unknown', False)
+    assert c1 == c2
 
 
 def test_cat_attributes():
-    pass
+    c3 = Cat('lion', 2.5, 'elephant', True)
+
+    assert c3.type == 'lion'
+    assert c3.max_size == 2.5
+    assert c3.favourite_food == 'elephant'
+    assert c3.roars is True
 
 
 def test_asdict():
-    pass
+    # asdict() should return a dictionary.
+    c_cat = Cat('tiger', 3.9, 'deer', True)
+    c_dict = c_cat._asdict()
+    expected = {'type': 'tiger',
+                'max_size': 3.9,
+                'favourite_food': 'deer',
+                'roars': True
+                }
+
+    assert c_dict == expected
 
 
 def test_cat_replace():
-    pass
+    # replace() should change passed in fields
+    c_before = Cat('cheetah', 1.5, 'rabbit', True)
+    c_after = c_before._replace(roars=False)
+    c_expected = Cat('cheetah', 1.5, 'rabbit', False)
+
+    assert c_after == c_expected
